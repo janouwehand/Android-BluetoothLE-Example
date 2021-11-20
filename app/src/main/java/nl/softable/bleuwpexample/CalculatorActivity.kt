30c6cb040c98f0d9e5ba5a584355482c342eecd4
J.L. Ouwehand <jan@softable.nl>
2021-11-20 22:02:27 +0100

    Calculator

--- app/src/main/java/nl/softable/bleuwpexample/CalculatorActivity.kt
+++ app/src/main/java/nl/softable/bleuwpexample/CalculatorActivity.kt
@@ -1,15 +1,22 @@
 package nl.softable.bleuwpexample
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothGattCharacteristic.FORMAT_UINT32
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import nl.softable.bleuwpexample.bt.ExampleCharacteristics
+import nl.softable.bleuwpexample.bt.ExampleWriteCharacteristic
+import nl.softable.bleuwpexample.databinding.ActivityCalculatorBinding
 
 class CalculatorActivity : AppCompatActivity() {
 
-    companion object{
+    companion object {
         val DEVICEADDRESS = "DEVICEADDRESS"
     }
 
     private lateinit var deviceAddress: String
+    lateinit var binding: ActivityCalculatorBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +24,29 @@ class CalculatorActivity : AppCompatActivity() {
 
         deviceAddress = intent.getStringExtra(DEVICEADDRESS)!!
         title = deviceAddress
+
+        binding = ActivityCalculatorBinding.inflate(layoutInflater)
     }
 
+    fun queryServer(view: View) {
+        var gatt = ExampleCharacteristics(
+            this,
+            BluetoothAdapter.getDefaultAdapter(),
+            Constants.serviceUUID,
+            deviceAddress
+        )
 
+        var valueOperandA = binding.edtOperandA.text.toString().toInt()
+        var valueOperandB = binding.edtOperandB.text.toString().toInt()
+        var valueOperator = binding.spinner.selectedItemPosition + 1
+
+        var writes = listOf(
+            ExampleWriteCharacteristic(Constants.operand1UUID, FORMAT_UINT32, valueOperandA),
+            ExampleWriteCharacteristic(Constants.operand2UUID, FORMAT_UINT32, valueOperandB),
+            ExampleWriteCharacteristic(Constants.operatorUUID, FORMAT_UINT32, valueOperator)
+        )
+
+        gatt.write(writes).await()
+
+    }
 }
